a2b507cf96649901882b8b36b62697dc2a9532a2
Arafi <arafi@pop-os.localdomain>
2020-07-04 17:37:29 +0700

    ver 1.1

--- bot_viewer.py
+++ bot_viewer.py
@@ -1,15 +1,41 @@
-#Version 1.0
+#Version 1.1
 #need to add proxy
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Firefox()
 count = 0
 
-while count < 1:
-    browser.get("https://youtu.be/Z_G_BpPzGIE")
+def login(t):
+    browser.get("https://stackoverflow.com/")
+    time.sleep(t)
+    login_button = browser.find_elements_by_xpath("/html/body/header/div/ol[2]/li[2]/a[1]")[0]
+    login_button.click()
+    time.sleep(t)
+    google_button = browser.find_elements_by_xpath("/html/body/div[4]/div[2]/div/div[2]/button[1]")[0]
+    google_button.click()
+    time.sleep(t)
+    acount_name = browser.find_elements_by_id("identifierId")[0]
+    acount_name.send_keys("naruto1745uzumaki")
+    acount_name.send_keys(Keys.ENTER)
+    time.sleep(t)
+    password = browser.find_elements_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")[0]
+    password.send_keys("arafi1745")
+    password.send_keys(Keys.ENTER)
+    time.sleep(15)
+
+def video(link, t):
+    browser.get(link)
     play_button = browser.find_elements_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[23]/div[2]/div[1]/button")[0]
     play_button.click()
-    time.sleep(130)
+    time.sleep(t)
     count = count + 1
+
+login(5)
+
+while count < 50:
+    video("https://youtu.be/Z_G_BpPzGIE", 130)
+    video("https://www.youtube.com/watch?v=VLpFgay9ZWY", 135)
+    video("https://www.youtube.com/watch?v=UFwQZ7od4tY", 200)
