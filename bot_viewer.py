a04336acda704888ce57a22eeaaa9d1a4e57cdef
Arafi <arafi@pop-os.localdomain>
2020-07-04 08:20:46 +0700

    ver 1.0

--- bot_viewer.py
+++ bot_viewer.py
@@ -1,5 +1,15 @@
 #Version 1.0
+#need to add proxy
+
 from selenium import webdriver
+import time
 
 browser = webdriver.Firefox()
-browser.get("https://youtube.com")
+count = 0
+
+while count < 1:
+    browser.get("https://youtu.be/Z_G_BpPzGIE")
+    play_button = browser.find_elements_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[23]/div[2]/div[1]/button")[0]
+    play_button.click()
+    time.sleep(130)
+    count = count + 1
